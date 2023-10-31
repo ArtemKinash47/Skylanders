6a648d42d77e2e11b39c3ca4cf9840686512c8b8
ArtemKinash47 <artema5216440@gmail.com>
2023-10-31 23:08:28 +0200

    update23.0831.10

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 var skylanders = [
-    {name: "Whirlwind", element: "Air", catchphrase: "Twists of Fury!", imageUrl: "whirlwind.png"},
-    {name: "Lightning Rod", element: "Air", catchphrase: "One Strike and You're Out!", imageUrl: "lightningrod.png"},
-    {name: "Sonic Boom", element: "Air", catchphrase: "Full Scream Ahead!", imageUrl: "sonicboom.png"},
-    {name: "Warnado", element: "Air", catchphrase: "For the Wind!", imageUrl: "warnado.png"},
-    {name: "Terrafin", element: "Earth", catchphrase: "It's Feeding Time!", imageUrl: "terrafin.png"},
-    {name: "Prism Break", element: "Earth", catchphrase: "The Beam is Supreme!", imageUrl: "prismbreak.png"},
-    {name: "Dino-Rang", element: "Earth", catchphrase: "Come 'Rang or Shine!", imageUrl: "dinorang.png"},
-    {name: "Bash", element: "Earth", catchphrase: "Rock and Roll!", imageUrl: "bash.png"},
-    {name: "Eruptor", element: "Fire", catchphrase: "Born to Burn!", imageUrl: "eruptor.png"},
-    {name: "Flameslinger", element: "Fire", catchphrase: "Let the Flames Begin!", imageUrl: "flameslinger.png"},
-    {name: "Ignitor", element: "Fire", catchphrase: "Slash and Burn!", imageUrl: "ignitor.png"},
-    {name: "Sunburn", element: "Fire", catchphrase: "Roast N' Toast!", imageUrl: "sunburn.webp"},
-    {name: "Zook", element: "Life", catchphrase: "Locked and Loaded!", imageUrl: "zook.webp"},
-    {name: "Stump Smash", element: "Life", catchphrase: "Drop The Hammer!", imageUrl: "stumpsmash.webp"},
-    {name: "Stealth Elf", element: "Life", catchphrase: "Silent but Deadly!", imageUrl: "stealthelf.webp"},
-    {name: "Camo", element: "Life", catchphrase: "Fruit Punch!", imageUrl: "camo.webp"},
-    {name: "Double Trouble", element: "Magic", catchphrase: "Boom Shock-A-Laka!", imageUrl: "doubletrouble.webp"},
-    {name: "Spyro", element: "Magic", catchphrase: "All Fired Up!", imageUrl: "spyro.webp"},
-    {name: "Voodood", element: "Magic", catchphrase: "Axe First, Questions Later!", imageUrl: "voodood.webp"},
-    {name: "Wrecking Ball", element: "Magic", catchphrase: "Wreck-N-Roll!", imageUrl: "wreckingball.webp"},
-    {name: "Boomer", element: "Tech", catchphrase: "Bring the Boom!", imageUrl: "boomer.webp"},
-    {name: "Drill Sergeant", element: "Tech", catchphrase: "Licensed to Drill!", imageUrl: "drillsergeant.webp"},
-    {name: "Drobot", element: "Tech", catchphrase: "Blink and Destroy!", imageUrl: "drobot.webp"},
-    {name: "Trigger Happy", element: "Tech", catchphrase: "No Gold, No Glory!", imageUrl: "triggerhappy.webp"},
-    {name: "Chop Chop", element: "Undead", catchphrase: "Slice and Dice!", imageUrl: "chopchop.webp"},
-    {name: "Cynder", element: "Undead", catchphrase: "Volts and Lightning!", imageUrl: "cynder.webp"},
-    {name: "Ghost Roaster", element: "Undead", catchphrase: "No Chain, No Gain! Ahahahahahaaa!", imageUrl: "ghostroaster.webp"},
-    {name: "Hex", element: "Undead", catchphrase: "Fear the Dark!", imageUrl: "hex.webp"},
-    {name: "Gill Grunt", element: "Water", catchphrase: "Fear the Fish!", imageUrl: "gillgrunt.webp"},
-    {name: "Slam Bam", element: "Water", catchphrase: "Armed and Dangerous!", imageUrl: "slambam.webp"},
-    {name: "Wham-Shell", element: "Water", catchphrase: "Brace for the Mace!", imageUrl: "whamshell.webp"},
-    {name: "Zap", element: "Water", catchphrase: "Ride the Lightning!", imageUrl: "zap.webp"},
+    {name: "Whirlwind", element: "Air", catchphrase: "Twists of Fury!", imageUrl: "whirlwind.png", game:  "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Lightning Rod", element: "Air", catchphrase: "One Strike and You're Out!", imageUrl: "lightningrod.png", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Sonic Boom", element: "Air", catchphrase: "Full Scream Ahead!", imageUrl: "sonicboom.png", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Warnado", element: "Air", catchphrase: "For the Wind!", imageUrl: "warnado.png", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Terrafin", element: "Earth", catchphrase: "It's Feeding Time!", imageUrl: "terrafin.png", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Prism Break", element: "Earth", catchphrase: "The Beam is Supreme!", imageUrl: "prismbreak.png", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Dino-Rang", element: "Earth", catchphrase: "Come 'Rang or Shine!", imageUrl: "dinorang.png", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Bash", element: "Earth", catchphrase: "Rock and Roll!", imageUrl: "bash.png", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Eruptor", element: "Fire", catchphrase: "Born to Burn!", imageUrl: "eruptor.png", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Flameslinger", element: "Fire", catchphrase: "Let the Flames Begin!", imageUrl: "flameslinger.png", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Ignitor", element: "Fire", catchphrase: "Slash and Burn!", imageUrl: "ignitor.png", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Sunburn", element: "Fire", catchphrase: "Roast N' Toast!", imageUrl: "sunburn.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Zook", element: "Life", catchphrase: "Locked and Loaded!", imageUrl: "zook.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Stump Smash", element: "Life", catchphrase: "Drop The Hammer!", imageUrl: "stumpsmash.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Stealth Elf", element: "Life", catchphrase: "Silent but Deadly!", imageUrl: "stealthelf.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Camo", element: "Life", catchphrase: "Fruit Punch!", imageUrl: "camo.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Double Trouble", element: "Magic", catchphrase: "Boom Shock-A-Laka!", imageUrl: "doubletrouble.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Spyro", element: "Magic", catchphrase: "All Fired Up!", imageUrl: "spyro.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Voodood", element: "Magic", catchphrase: "Axe First, Questions Later!", imageUrl: "voodood.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Wrecking Ball", element: "Magic", catchphrase: "Wreck-N-Roll!", imageUrl: "wreckingball.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Boomer", element: "Tech", catchphrase: "Bring the Boom!", imageUrl: "boomer.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Drill Sergeant", element: "Tech", catchphrase: "Licensed to Drill!", imageUrl: "drillsergeant.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Drobot", element: "Tech", catchphrase: "Blink and Destroy!", imageUrl: "drobot.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Trigger Happy", element: "Tech", catchphrase: "No Gold, No Glory!", imageUrl: "triggerhappy.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Chop Chop", element: "Undead", catchphrase: "Slice and Dice!", imageUrl: "chopchop.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Cynder", element: "Undead", catchphrase: "Volts and Lightning!", imageUrl: "cynder.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Ghost Roaster", element: "Undead", catchphrase: "No Chain, No Gain! Ahahahahahaaa!", imageUrl: "ghostroaster.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Hex", element: "Undead", catchphrase: "Fear the Dark!", imageUrl: "hex.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Gill Grunt", element: "Water", catchphrase: "Fear the Fish!", imageUrl: "gillgrunt.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Slam Bam", element: "Water", catchphrase: "Armed and Dangerous!", imageUrl: "slambam.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Wham-Shell", element: "Water", catchphrase: "Brace for the Mace!", imageUrl: "whamshell.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
+    {name: "Zap", element: "Water", catchphrase: "Ride the Lightning!", imageUrl: "zap.webp", game: "Skylanders: Spyro's Adventures", type: "skylander"},
 
 ];
 
@@ -47,17 +47,24 @@ function checkGuess() {
         attempts++;
         result.textContent = "Incorrect. Try again!";
         if(attempts >= 2) {
-            result.textContent = "Hint: The Skylander's catchphrase is '" + randomSkylander.catchphrase + "'.";
+            result.textContent = "Hint: The Skylander's type is " + randomSkylander.type + ".";
             if(attempts >= 4) {
                 result.textContent = "Another hint: The Skylander's element is " + randomSkylander.element + ".";
             }
             if(attempts >= 6) {
-                result.textContent = "Another hint: The Skylander's name starts with '" + randomSkylander.name.charAt(0) + "'.";
+                result.textContent = "Another hint: The Skylander's game where he was created  " +  randomSkylander.game  +    ".";
             }
             if(attempts >= 8) {
-                result.textContent = "Final hint: Here is an image of the Skylander.";
-                displaySkylanderImage(randomSkylander);
+                result.textContent = "Another hint: Skylander's catchphrase is: " + randomSkylander.catchphrase + ".";
             }
+            if(attempts >= 10) {
+                result.textContent = "Another hint: First letter of skylander's name is " + randomSkylander.name.charAt(0) + ".";
+            }
+            if(attempts >= 12) {
+                result.textContent = "Final hint: Skylander's image looks like this: ";
+                displaySkylanderImage(randomSkylander)
+            }
+
         }
     }
 }
